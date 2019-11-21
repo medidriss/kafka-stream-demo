c49be3ed7a5149981127d3ace834838e2d9555d2
medidriss <idriss.mohamed123@gmail.com>
2019-11-21 19:31:40 +0100

    kafka stream demo

---
+++ src/main/java/com/idriss/KafkaStreamDemoApplication.java
@@ -0,0 +1,13 @@
+package com.idriss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idriss/KafkaStreamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idriss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
